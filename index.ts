ba32b25b4cada1c2b7ca6d8592bc100c14894056
AREEBA-FATIMA <areebafatima2457@gmail.com>
2024-04-05 17:18:37 +0500

    first commit

---
+++ index.ts
@@ -0,0 +1,33 @@
+#!/usr/bin/env node
+
+import inquirer from "inquirer";
+
+async function inputData() {
+    let userInput = await inquirer.prompt([
+        {
+            name: "paragraph",
+            type: "input",
+            message: "Please write a paragraph..."
+        },
+        {
+            name: "countType",
+            message: "Please select one",
+            type: "list",
+            choices: ["Count the words", "Count the letters"]
+        }
+    ]);
+
+    if (userInput.countType === "Count the letters") {
+        let paragraph = userInput.paragraph;
+        let removeWhiteSpaces = paragraph.replace(/\s/g, "");
+        let lengthWithoutSpaces = removeWhiteSpaces.length;
+        console.log(`The total characters without space is ${lengthWithoutSpaces}`);
+    } else if (userInput.countType === "Count the words") {
+        let paragraph = userInput.paragraph;
+        let words = paragraph.split(/\s+/);
+        let totalWords = words.length;
+        console.log(`The total number of words in the paragraph is ${totalWords}`);
+    }
+}
+
+inputData();
